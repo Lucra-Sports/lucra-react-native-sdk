87d1dbe2351018921540d87caf8f02d55dc0dcfc
Michael Schmidt <mike.schmidt@lucrasports.com>
2023-08-14 19:51:16 -0400

    renaming module name from react-native-lucrasdk to lucra-react-native-sdk

--- src/LucraClient.tsx
+++ src/LucraClient.tsx
@@ -2,7 +2,7 @@ import React, { type PropsWithChildren } from 'react';
 import { NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
-  `The package 'react-native-lucrasdk' doesn't seem to be linked. Make sure: \n\n` +
+  `The package 'lucra-react-native-sdk' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
