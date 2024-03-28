7069429e8deb874537a6d50b45a4473bc1da54d9
Oscar Franco <ospfranco@gmail.com>
2024-03-28 13:43:03 +0100

    Replace credentials

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,36 +1,9 @@
 import { LucraSDK } from '@lucra-sports/lucra-react-native-sdk';
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes } from './Routes';
 import { Platform, StatusBar } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-
-LucraSDK.init({
-  apiURL: 'https://api.sandbox.lucra.com',
-  apiKey: 'YGugBV5xGsicmp48syEcDlBUQ98YeHE5',
-  environment: LucraSDK.ENVIRONMENT.SANDBOX,
-  theme: {
-    background: '#001448',
-    surface: '#1C2575',
-    primary: '#09E35F',
-    secondary: '#5E5BD0',
-    tertiary: '#9C99FC',
-    onBackground: '#FFFFFF',
-    onSurface: '#FFFFFF',
-    onPrimary: '#001448',
-    onSecondary: '#FFFFFF',
-    onTertiary: '#FFFFFF',
-    fontFamily:
-      Platform.OS === 'ios'
-        ? 'Inter'
-        : {
-            normal: 'Inter-Regular',
-            bold: 'Inter-Bold',
-            semibold: 'Inter-SemiBold',
-            medium: 'Inter-Medium',
-          },
-  },
-});
 
 if (Platform.OS === 'ios') {
   // User callback is currently only supported on iOS
@@ -40,6 +13,42 @@ if (Platform.OS === 'ios') {
 }
 
 export default function App() {
+  const [isReady, setIsReady] = React.useState(false);
+  useEffect(() => {
+    LucraSDK.init({
+      apiURL: 'api-sample.sandbox.lucrasports.com',
+      apiKey: 'YGugBV5xGsicmp48syEcDlBUQ98YeHE5',
+      environment: LucraSDK.ENVIRONMENT.SANDBOX,
+      theme: {
+        background: '#001448',
+        surface: '#1C2575',
+        primary: '#09E35F',
+        secondary: '#5E5BD0',
+        tertiary: '#9C99FC',
+        onBackground: '#FFFFFF',
+        onSurface: '#FFFFFF',
+        onPrimary: '#001448',
+        onSecondary: '#FFFFFF',
+        onTertiary: '#FFFFFF',
+        fontFamily:
+          Platform.OS === 'ios'
+            ? 'Inter'
+            : {
+                normal: 'Inter-Regular',
+                bold: 'Inter-Bold',
+                semibold: 'Inter-SemiBold',
+                medium: 'Inter-Medium',
+              },
+      },
+    }).then(() => {
+      setIsReady(true);
+    });
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <LinearGradient colors={['#6360EB', '#001448']} className="h-full">
