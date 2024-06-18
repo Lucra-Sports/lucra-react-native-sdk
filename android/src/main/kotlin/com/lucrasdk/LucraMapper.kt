ae50b03d81032ceadbf02a0f6aea44673ee8bfa1
Elliot Mitchell <elliot.r.mitchell@gmail.com>
2024-06-18 15:43:30 -0500

    Updated kotlin mappers to be null friendly

--- android/src/main/kotlin/com/lucrasdk/LucraMapper.kt
+++ android/src/main/kotlin/com/lucrasdk/LucraMapper.kt
@@ -16,81 +16,81 @@ import com.lucrasports.sports_contests.LucraTeam
 
 object LucraMapper {
 
-  fun sportIntervalsToMap(interval: SportsInterval): Map<String, Any> {
-    val map = mutableMapOf<String, Any>()
+  fun sportIntervalsToMap(interval: SportsInterval): Map<String, Any?> {
+    val map = mutableMapOf<String, Any?>()
     map["interval"] = interval.interval
     map["displayName"] = interval.displayName
     return map
   }
 
-  fun sportToMap(sport: LucraSport): Map<String, Any> {
-    val map = mutableMapOf<String, Any>()
+  fun sportToMap(sport: LucraSport): Map<String, Any?> {
+    val map = mutableMapOf<String, Any?>()
     map["id"] = sport.id
     map["name"] = sport.name
     map["iconUrl"] = sport.iconUrl
     map["priority"] = sport.priority
     map["leagues"] = sport.leagues
-    map["intervals"] = sport.intervals.map(::sportIntervalsToMap) as Any
+    map["intervals"] = sport.intervals.map(::sportIntervalsToMap) as? Any
     return map
   }
 
-  fun leagueToMap(league: LucraLeague?): Map<String, Any>? {
+  fun leagueToMap(league: LucraLeague?): Map<String, Any?>? {
     if (league == null) {
       return null
     }
 
-    val map = mutableMapOf<String, Any>()
+    val map = mutableMapOf<String, Any?>()
     map["id"] = league.id
     map["name"] = league.name
     map["logoUrl"] = league.logoUrl
     map["priority"] = league.priority
-    map["schedules"] = league.schedules?.map(::scheduleToMap) as Any
+    map["schedules"] = league.schedules?.map(::scheduleToMap) as? Any
     return map
   }
 
-  fun playerStatToMap(stat: LucraPlayerStat?): Map<String, Any>? {
+  fun playerStatToMap(stat: LucraPlayerStat?): Map<String, Any?>? {
     if (stat == null) {
       return null
     }
 
-    val map = mutableMapOf<String, Any>()
+    val map = mutableMapOf<String, Any?>()
     map["metricId"] = stat.metricId
-    map["value"] = stat.value as Any
+    map["value"] = stat.value as? Any
     return map
   }
 
-  fun playerToMap(player: LucraPlayer): Map<String, Any> {
-    val map = mutableMapOf<String, Any>()
+  fun playerToMap(player: LucraPlayer): Map<String, Any?> {
+    val map = mutableMapOf<String, Any?>()
     map["id"] = player.id
     map["firstName"] = player.firstName
     map["lastName"] = player.lastName
-    map["headshottUrl"] = player.headshotUrl as Any
+    map["headshottUrl"] = player.headshotUrl as? Any
     map["lucraPosition"] = player.lucraPosition
     map["position"] = player.position
     map["positionAbbreviation"] = player.positionAbbreviation
     map["status"] = player.status
     map["isAvailable"] = player.isAvailable
     map["sport"] = sportToMap(player.sport)
-    map["positionMetrics"] = player.positionMetrics?.map(::metricToMap) as Any
-    map["projectedStats"] = player.projectedStats?.map(::playerStatToMap) as Any
-    map["seasonAvgStats"] = player.seasonAvgStats?.map(::playerStatToMap) as Any
-    map["liveGameStats"] = player.liveGameStats?.map(::playerStatToMap) as Any
-    map["team"] = teamToMap(player.team) as Any
-    map["league"] = leagueToMap(player.league) as Any
-    map["ranking"] = player.ranking as Any
-    map["schedule"] = scheduleToMap(player.schedule) as Any
+    map["positionMetrics"] = player.positionMetrics?.map(::metricToMap) as? Any
+    map["projectedStats"] = player.projectedStats?.map(::playerStatToMap) as? Any
+    map["seasonAvgStats"] = player.seasonAvgStats?.map(::playerStatToMap) as? Any
+    map["liveGameStats"] = player.liveGameStats?.map(::playerStatToMap) as? Any
+    map["team"] = teamToMap(player.team) as? Any
+    map["league"] = leagueToMap(player.league) as? Any
+    map["ranking"] = player.ranking as? Any
+    map["schedule"] = scheduleToMap(player.schedule) as? Any
 
     return map
   }
 
-  fun teamToMap(team: LucraTeam?): Map<String, Any>? {
+  fun teamToMap(team: LucraTeam?): Map<String, Any?>? {
     if (team == null) {
       return null
     }
 
-    val map = mutableMapOf<String, Any>()
+    val map = mutableMapOf<String, Any?>()
     map["id"] = team.id
-    map["logoUrl"] = team.logoUrl as Any
+    map["logoUrl"] = team.logoUrl as? Any
     map["fullName"] = team.fullName
     map["name"] = team.name
     map["sport"] = sportToMap(team.sport)
@@ -98,38 +98,38 @@ object LucraMapper {
     return map
   }
 
-  fun scheduleToMap(schedule: LucraSchedule?): Map<String, Any>? {
+  fun scheduleToMap(schedule: LucraSchedule?): Map<String, Any?>? {
     if (schedule == null) {
       return null
     }
 
-    val map = mutableMapOf<String, Any>()
+    val map = mutableMapOf<String, Any?>()
     map["id"] = schedule.id
     map["date"] = schedule.date
-    map["channel"] = schedule.channel as Any
+    map["channel"] = schedule.channel as? Any
     map["status"] = schedule.status.rawValue
-    map["homeTeam"] = teamToMap(schedule.homeTeam) as Any
-    map["awayTeam"] = teamToMap(schedule.awayTeam) as Any
-    map["players"] = schedule.players?.map(::playerToMap) as Any
+    map["homeTeam"] = teamToMap(schedule.homeTeam) as? Any
+    map["awayTeam"] = teamToMap(schedule.awayTeam) as? Any
+    map["players"] = schedule.players?.map(::playerToMap) as? Any
     map["venue"] = schedule.venue
-    map["roundName"] = schedule.roundName as Any
-    map["statusDescription"] = schedule.statusDescription as Any
-    map["homeScore"] = schedule.homeScore as Any
-    map["awayScore"] = schedule.awayScore as Any
+    map["roundName"] = schedule.roundName as? Any
+    map["statusDescription"] = schedule.statusDescription as? Any
+    map["homeScore"] = schedule.homeScore as? Any
+    map["awayScore"] = schedule.awayScore as? Any
     map["sport"] = schedule.sport
-    map["projectionsPending"] = schedule.projectionsPending as Any
+    map["projectionsPending"] = schedule.projectionsPending as? Any
     return map
   }
 
-  fun metricToMap(metric: LucraMetric?): Map<String, Any>? {
+  fun metricToMap(metric: LucraMetric?): Map<String, Any?>? {
     if (metric == null) {
       return null
     }
 
-    val map = mutableMapOf<String, Any>()
+    val map = mutableMapOf<String, Any?>()
     map["id"] = metric.id
     map["displayName"] = metric.displayName
-    map["pluralDisplayName"] = metric.pluralDisplayName as Any
+    map["pluralDisplayName"] = metric.pluralDisplayName as? Any
     map["shortName"] = metric.shortName
     map["maxValue"] = metric.maxValue
     map["active"] = metric.active
@@ -138,40 +138,40 @@ object LucraMapper {
     return map
   }
 
-  fun userToMap(user: LucraUser): Map<String, Any> {
-    val map = mutableMapOf<String, Any>()
+  fun userToMap(user: LucraUser): Map<String, Any?> {
+    val map = mutableMapOf<String, Any?>()
     map["id"] = user.id
-    map["socialConnectionId"] = user.socialConnectionId as Any
+    map["socialConnectionId"] = user.socialConnectionId as? Any
     map["username"] = user.username
-    map["avatarUrl"] = user.avatar_url as Any
+    map["avatarUrl"] = user.avatar_url as? Any
     map["loyaltyPoints"] = user.loyalty_points
     return map
   }
 
-  fun matchupTeamUserToMap(user: MatchupType.MatchupTeamUser): Map<String, Any> {
-    val map = mutableMapOf<String, Any>()
+  fun matchupTeamUserToMap(user: MatchupType.MatchupTeamUser): Map<String, Any?> {
+    val map = mutableMapOf<String, Any?>()
     map["id"] = user.user.id
     map["user"] = userToMap(user.user)
     map["wagerPercentage"] = user.wagerPercentage
     return map
   }
 
-  fun sportMatchupTeamToMap(team: SportsMatchupTeam): Map<String, Any> {
-    val map = mutableMapOf<String, Any>()
+  fun sportMatchupTeamToMap(team: SportsMatchupTeam): Map<String, Any?> {
+    val map = mutableMapOf<String, Any?>()
     map["id"] = team.id
     map["users"] = team.users.map(::matchupTeamUserToMap)
     map["outcome"] = team.outcome.toString()
     map["player"] = playerToMap(team.player)
-    map["schedule"] = scheduleToMap(team.schedule) as Any
-    map["metric"] = metricToMap(team.metric) as Any
-    map["metricValue"] = team.metricValue as Any
+    map["schedule"] = scheduleToMap(team.schedule) as? Any
+    map["metric"] = metricToMap(team.metric) as? Any
+    map["metricValue"] = team.metricValue as? Any
     map["spread"] = team.spread
     map["wagerAmount"] = team.wagerAmount
     return map
   }
 
-  fun sportsMatchupToMap(matchup: SportsMatchupType): Map<String, Any> {
-    val map = mutableMapOf<String, Any>()
+  fun sportsMatchupToMap(matchup: SportsMatchupType): Map<String, Any?> {
+    val map = mutableMapOf<String, Any?>()
     map["id"] = matchup.id
     map["createdAt"] = matchup.createdAt
     map["updatedAt"] = matchup.updatedAt
