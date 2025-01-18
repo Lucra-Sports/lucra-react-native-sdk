05d3ae0798c56ac17b8b73d62b71a042a58457a8
Oscar Franco <ospfranco@gmail.com>
2025-01-18 17:01:07 +0100

    Fix ts error

--- example/src/container/UIFlow.container.tsx
+++ example/src/container/UIFlow.container.tsx
@@ -60,7 +60,10 @@ export const UIFlowContainer: React.FC<Props> = ({ navigation }) => {
         <TouchableOpacity
           className="w-full border border-indigo-500 bg-indigo-700 p-4 items-center justify-center rounded-lg "
           onPress={() =>
-            LucraSDK.present({ name: LucraSDK.FLOW.VERIFY_IDENTITY })
+            LucraSDK.present({
+              name: LucraSDK.FLOW.VERIFY_IDENTITY,
+              verificationProcedure: 'ageAssuranceVerification',
+            })
           }
         >
           <Text className="font-bold text-white">Verify Identity</Text>
