1cb8adb7046155e34bc197557821c7dde6e0e0a3
Oscar Franco <ospfranco@gmail.com>
2025-03-20 09:12:10 +0100

    WIP

--- scripts/run-e2e-android.sh
+++ scripts/run-e2e-android.sh
@@ -3,26 +3,8 @@ set -x
 
 cd example
 
-cd android
-
-# Run assembleDebug and capture logs
-if ! ./gradlew assembleDebug --stacktrace --info > assembleDebug.log 2>&1; then
-  echo "❌ Error: Failed to assemble debug build"
-  cat assembleDebug.log
-  exit 1
-fi
-
-
-echo "🟦 Assemble debug done!"
-
-./gradlew assembleAndroidTest -DtestBuildType=debug
-
-echo "🟦 Assemble android test done!"
-
 yarn start > metro.log 2>&1 &
 METRO_BUNDLER_PID=$!
-
-adb wait-for-device
 
 yarn e2e:run-android --headless
 DETOX_EXIT_CODE=$?
