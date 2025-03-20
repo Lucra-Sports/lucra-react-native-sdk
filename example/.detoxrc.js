644b5052c7725dcae9b67fd82f429e5345ff3fbf
Rodrigo Lopez <rflopezm@gmail.com>
2025-03-20 01:32:34 -0400

    update emulator target

--- example/.detoxrc.js
+++ example/.detoxrc.js
@@ -50,7 +50,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Medium_Phone_API_34',
+        avdName: 'Medium_Phone_API_29',
       },
     },
   },
