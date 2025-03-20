c9b4c00cc97b9543627684d4d1714ce9b1507c9a
Wellison Pereira <wellison@lucrasports.com>
2025-03-20 11:31:12 -0400

    specify emulator, revert run-e2e-android.sh changes

--- example/.detoxrc.js
+++ example/.detoxrc.js
@@ -50,7 +50,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Medium_Phone_API_29',
+        avdName: 'arm64emulator',
       },
     },
   },
