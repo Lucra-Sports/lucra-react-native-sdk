80bb5dedf5d29ac7770f7d1104fd5485a3e53677
Rodrigo Lopez <rflopezm@gmail.com>
2025-03-20 01:06:47 -0400

    align api level, update android script

--- scripts/run-e2e-android.sh
+++ scripts/run-e2e-android.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
+set -e
 
 cd example
 
-yarn start &
-
+yarn start > metro.log 2>&1 &
 METRO_BUNDLER_PID=$!
 
-yarn e2e:build-android
+adb wait-for-device
 
 yarn e2e:run-android --headless
-
 DETOX_EXIT_CODE=$?
 
-kill $METRO_BUNDLER_PID
+kill $METRO_BUNDLER_PID || true
+wait $METRO_BUNDLER_PID 2>/dev/null || true
 
 exit $DETOX_EXIT_CODE
