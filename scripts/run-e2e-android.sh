64dc8027d66ca527adcec66b5fc7feed6c5eab12
Wellison Pereira <wellison@lucrasports.com>
2025-03-20 11:27:20 -0400

    add android runner label to ci.yml, target arm64 script

--- scripts/run-e2e-android.sh
+++ scripts/run-e2e-android.sh
@@ -6,10 +6,14 @@ cd example
 yarn start > metro.log 2>&1 &
 METRO_BUNDLER_PID=$!
 
+emulator -avd my-arm64-avd -no-snapshot -no-window &
+adb wait-for-device
+
 yarn e2e:run-android --headless
 DETOX_EXIT_CODE=$?
 
 kill $METRO_BUNDLER_PID || true
 wait $METRO_BUNDLER_PID 2>/dev/null || true
+killall -9 qemu-system-arm || true
 
 exit $DETOX_EXIT_CODE
